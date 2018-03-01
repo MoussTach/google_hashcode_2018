8c42f2a200d7d5ab7809ae1990c5eb7af913437b
Unknown <corentin.dlplq@gmail.com>
2018-03-01 21:09:33 +0100

    algo for directions

--- src/main/java/eu/pandaroux/hashcode/application/Main.java
+++ src/main/java/eu/pandaroux/hashcode/application/Main.java
@@ -1,21 +1,22 @@
 package eu.pandaroux.hashcode.application;
 
 import eu.pandaroux.hashcode.algorithm.Essai_class;
+import eu.pandaroux.hashcode.algorithm.Logic_Loop;
 
 public class Main {
 
     public static void main(String[] args)
     {
-        Essai_class essai = new Essai_class("je veux stocker cette chaine de caractere", 42);
+            Logic_Loop testlogicloop = new Logic_Loop(
+                    new int[] {1, 1},
+                    new int[] {4, 3},
+                    1,
+                    6,
+                    -3,
+                    -2,
+                    3,
+                    2);
 
-        System.out.println(essai.getLaChaine() + " : " + essai.getLeNombre());
-
-        essai.setLaChaine("je change la chaine de la classe en utilisant un setter");
-
-        //ceci est un commentaire
-        //Pareil pour les nombres
-        essai.setLeNombre(84);
-
-        System.out.println(essai.getLaChaine() + " : " + essai.getLeNombre());
+            testlogicloop.algo();
     }
 }
