5a0b7eb3cf1f4d863e07b7b132f9877a73bafcb5
leo <lelefornes@live.fr>
2018-03-01 21:38:46 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/eu/pandaroux/hashcode/algorithm/Essai_class.java
@@ -0,0 +1,30 @@
+package eu.pandaroux.hashcode.algorithm;
+
+
+public class Essai_class {
+
+    private String onStockDansLaClasse;
+    private int iciAussi;
+
+    public Essai_class(String ceciEstUneVariable, int ceciEstUneAutreVariable)
+    {
+        onStockDansLaClasse = ceciEstUneVariable;
+        iciAussi = ceciEstUneAutreVariable;
+    }
+
+    public String getLaChaine() {
+        return onStockDansLaClasse;
+    }
+
+    public int getLeNombre() {
+        return iciAussi;
+    }
+
+    public void setLaChaine(String onStockDansLaClasse) {
+        this.onStockDansLaClasse = onStockDansLaClasse;
+    }
+
+    public void setLeNombre(int iciAussi) {
+        this.iciAussi = iciAussi;
+    }
+}
